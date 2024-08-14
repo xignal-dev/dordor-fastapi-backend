4ff91ec69f195eda2197d137b4920f296ed1ec36
keonlee <keonlee.gun@gmail.com>
2024-08-14 14:26:20 +0900

    fix: 파일 정리 중

--- api/api_v1/api.py
+++ api/api_v1/api.py
@@ -1,14 +1,6 @@
 from fastapi import APIRouter
 
-from api.api_v1.endpoints import chroma, milvus, qdrant, faiss
+from api.api_v1.endpoints import users
 
 api_router = APIRouter()
-api_router.include_router(chroma.router)
-# api_router.include_router(chroma.router, tags=["chroma"])
-
-# api_router.include_router(chromaKr.router, tags=["chromaKr"])
-# api_router.include_router(chromaEn.router, tags=["chromaEn"])
-
-# api_router.include_router(milvus.router, tags=["milvus"])
-# api_router.include_router(qdrant.router, tags=["qdrant"])
-# api_router.include_router(faiss.router, tags=["faiss"])
+api_router.include_router(users.router)
---
+++ api/api_v1/endpoints/users.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter, Body, Depends, HTTPException, UploadFile, Request
+
+router = APIRouter()
